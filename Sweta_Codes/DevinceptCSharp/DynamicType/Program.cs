45713a7a21b0edd20f6890ac9d4c126604a500d3
Sweta Rani <swetasr1509@gmail.com>
2021-08-01 23:14:16 +0530

    Added Codes

---
+++ Sweta_Codes/DevinceptCSharp/DynamicType/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DynamicType
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			dynamic am = "00:00";
+			dynamic pm = "12:00";
+			dynamic nowTime = DateTime.Now;
+
+			Console.WriteLine("AM starts from {0} whose type is {1}", am, am.GetType());
+			Console.WriteLine("PM starts from {0} whose type is {1}", pm, pm.GetType());
+			Console.WriteLine("Current time is {0} whose type is {1}", nowTime, nowTime.GetType());
+		}
+	}
+}
---
+++ Sweta_Codes/DevinceptCSharp/OperatorOverloading/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace OperatorOverloading
+{
+	class Program
+	{
+		int feet, inch;
+		public Program()
+		{
+			this.feet = 0;
+			this.inch = 0;
+		}
+
+		public Program(int feet, int inch)
+		{
+			this.feet = feet;
+			this.inch = inch;
+		}
+
+		public static Program operator +(Program l1, Program l2)
+		{
+			Program l3 = new Program();
+			l3.feet = l1.feet + l2.feet;
+			l3.inch = l1.inch + l2.inch;
+			if (l3.inch >= 12)
+			{
+				l3.feet++;
+				l3.inch -= 12;
+			}
+			return l3;
+		}
+		string GetLength()
+		{
+			return String.Format("Length : {0}' {1}\"", feet, inch);
+		}
+
+		static void Main(string[] args)
+		{
+			Program len1 = new Program(2, 8);
+			Program len2 = new Program(4, 5);
+			Program len3 = len1 + len2;
+
+			Console.WriteLine(len1.GetLength());
+			Console.WriteLine(len2.GetLength());
+		}
+	}
+}
